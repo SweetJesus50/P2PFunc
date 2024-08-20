cf72c0b6a45976ca6fd9cd3b91dad6032315658d
SweetJesus <methoxyn50@gmail.com>
2024-08-21 00:23:08 +0500

    redesign

--- wrappers/P2p.ts
+++ wrappers/P2p.ts
@@ -1,9 +1,29 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type P2pConfig = {};
+export type P2pConfig = {
+    arbitratorAddress: Address,
+    lessorAddress: Address,
+    renterAddress: Address,
+    content: Cell,
+    cost: bigint,
+    arbitratorFeePercent: number,
+    rentTime: number
+};
 
 export function p2pConfigToCell(config: P2pConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+                .storeAddress(config.arbitratorAddress)
+                .storeAddress(config.lessorAddress)
+                .storeAddress(config.renterAddress)
+                .storeRef(config.content)
+                .storeRef(
+                    beginCell()
+                        .storeCoins(config.cost)
+                        .storeUint(config.arbitratorFeePercent, 32)
+                        .storeUint(config.rentTime, 32)
+                    .endCell()
+                )
+            .endCell();
 }
 
 export class P2p implements Contract {
@@ -25,5 +45,13 @@ export class P2p implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendDeposit(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0, 32).storeStringTail("Deposit").endCell()
+        })
     }
 }
